c4210b1c640fc10d4d92ae6ff3e3001c937d0fa0
Gregory <gregory.nicholas.mayer@hotmail.com>
2020-06-16 16:21:00 -0300

    Refactoring code using partials

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: {minimum:5, maximum:200}
-  validates :description, presence: true, length: {minimum:10}
+  validates :title, presence: {message: 'Specify a name, please'}, length: {in: 5..40}, uniqueness: true
+  validates :description, presence: true, length: {minimum:10, too_short: 'too short. Try giving more details!'}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  root 'welcome#home'
+  get 'about', to: 'welcome#about'
+  resources :articles
 end

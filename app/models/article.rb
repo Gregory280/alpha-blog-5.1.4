a7d1cb3fc26a90079c5ed9e992f6a9b8fb8e7364
Gregory <gregory.nicholas.mayer@hotmail.com>
2020-06-20 14:43:55 -0300

    Add one-to-many association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: {message: 'Specify a name, please'}, length: {in: 5..40}, uniqueness: true
   validates :description, presence: true, length: {minimum:10, too_short: 'too short. Try giving more details!'}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :articles
   validates :username, presence: true, length: {minimun: 3, maximum: 20}, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 100}, format: {with: VALID_EMAIL_REGEX}

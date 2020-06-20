a20f8f813be723cbdc8f8c9948912c0dfb563b81
Gregory <gregory.nicholas.mayer@hotmail.com>
2020-06-20 13:37:05 -0300

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-
+  validates :username, presence: true, length: {minimun: 3, maximum: 20}, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 100}, format: {with: VALID_EMAIL_REGEX}
 end

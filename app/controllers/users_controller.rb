b1b0da974575896995e0fc4ca3183fc7465822fa
Gregory <gregory.nicholas.mayer@hotmail.com>
2020-06-25 14:01:04 -0300

    Bunch of things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
@@ -7,7 +9,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Wlecome to the Alpha BLog, #{@user.username}"
+      session[:user_id] = @user.id
+      flash[:notice] = "Welecome to the Alpha BLog, #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
@@ -15,24 +18,35 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
+   
     if @user.update(user_params)
       flash[:notice] = "User was update successfully!"
-      redirect_to articles_path
+      redirect_to user_path
     else
       render 'edit'
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 4)
+  end
 
+  def index
+    @users = User.paginate(page: params[:page], per_page: 4)
+  end
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
